8dcf9f53723c422c9864aafab0e6ae6921042c79
Wilhuff-Tarkin <giantcaterpillar@gmail.com>
2019-03-23 09:56:46 +0100

    set up root

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 
     public static void main(String[] args) {
-        
+
+        launch(args);
     }
+
+    public void start (Stage primaryStage) throws Exception {
+
+    }
+
 }
